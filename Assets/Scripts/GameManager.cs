36fecf7dbe63710eac140f55b06ff6539677395c
Ilya Kositsin <ikositsin.99@gmail.com>
2020-02-28 21:26:27 +0400

    Done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+	void Start () {
+		GetComponent<AudioSource> ().Play ();
+		DontDestroyOnLoad (this.gameObject);
+		SceneManager.LoadScene ("Menu");
+	}
+}
